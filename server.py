96049e32af4ed2a498e98eddae4955b8ba30adc6
AmalSaidi <59490844+AmalSaidi@users.noreply.github.com>
2022-03-27 19:23:22 +0200

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 import threading
 
 host = '127.0.0.1'
-port = 55571
+port = 55573
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host,port))
@@ -20,12 +20,18 @@ def broadcast(message):
     for client in clients:
         client.send(message)
 
-
 def handle(client):
     while True:
         try:
-            message = client.recv(1024)
-            broadcast(message)
+            msg = message = client.recv(1024)
+            if msg.decode('ascii').startswith('QUIT'):
+                quitting_user = msg.decode('ascii')[5:]
+                quit_user(quitting_user)
+            elif msg.decode('ascii').startswith('LIST'):
+                requesting_user = msg.decode('ascii')[5:]
+                liste()
+            else:
+                broadcast(message)
         except:
             index = clients.index(client)
             clients.remove(client)
@@ -39,10 +45,12 @@ def handle(client):
 def receive():
     while True:
         client, address = server.accept()
-        print(f'Conncted with {str(address)}')
+        print(f'Connected with {str(address)}')
 
+        client.send('nickname?'.encode('ascii'))
         nickname = client.recv(1024).decode('ascii')
-
+        if nickname == 'amal':
+            print("hello admin")
         nicknames.append(nickname)
         clients.append(client)
         print(f'Nickname of the client is {nickname} !')
@@ -51,6 +59,20 @@ def receive():
 
         thread = threading.Thread(target=handle,args=(client,))
         thread.start()
+
+
+def quit_user(nom):
+    '''name_index = nicknames.index(nom)
+    client_to_quit = clients[name_index]
+    clients.remove(client_to_quit)
+    client_to_quit.send('you quit the session'.encode('ascii'))
+    client_to_quit.close()
+    nicknames.remove(nom)
+    broadcast(f'{nom} quit the server'.encode('ascii'))'''
+
+def liste():
+    print(nicknames)
+
 
 print("server is listening ...")
 receive()
