ba08c95ee96973c9eb97d7171acd59d4ab96fc0d
AmalSaidi <59490844+AmalSaidi@users.noreply.github.com>
2022-04-07 15:05:56 +0200

    Update server.py

--- server.py
+++ server.py
@@ -32,6 +32,7 @@ class Server:
                 #print(client)
                 if commande[1] == 'QUIT':
                     self.quit(client)
+                    boo = false
                 elif commande[1] == 'LIST':
                     self.liste_clients(client)
 
@@ -43,24 +44,24 @@ class Server:
 
                 elif commande[1] == 'CHAT':
                     msg = message.split(" ",1)
-                    self.broadcast(self.clients[client] + " : " + commande[2])
+                    self.broadcast("200 : " + self.clients[client] + " : " + commande[2])
 
                 elif commande[1] == 'SEND':
                     print(f'mon tableau: {commande} et sa taille: {len(commande)}')
 
                     if len(commande) < 3:
-                        client.send("Missing parameter".encode())
+                        client.send("418 ! Missing parameter".encode())
                     elif commande[2] == " ":
-                        client.send("Missing parameter".encode())
+                        client.send("418 : Missing parameter".encode())
                     else:
                         self.send(client,commande[2])
 
                 elif commande[1] == 'REFUSE':
                     print(f'mon tableau: {commande} et sa taille: {len(commande)}')
                     if len(commande) < 3:
-                        client.send("Missing parameter".encode())
+                        client.send("418 : Missing parameter".encode())
                     elif commande[2] == '':
-                        client.send("Missing parameter".encode())
+                        client.send("418 : Missing parameter".encode())
                     else:
                         self.refuse(client,commande[2])
 
@@ -72,14 +73,14 @@ class Server:
                     self.absent(client)
 
                 elif commande[1] == 'BACK':
-                    message="You are already online"
+                    message="418 : You are already online"
                     client.send(message.encode())
 
                 elif commande[1] == 'SFIC':
                     self.send_file(client)
 
                 else:
-                    client.send('the command was not found'.encode('ascii'))
+                    client.send('404 : the command was not found'.encode('ascii'))
             except:
                 client.close()
                 nickname = self.clients[client]
@@ -103,20 +104,20 @@ class Server:
             thread.start()
 
     def absent(self,client):
-        self.broadcast(f'{self.clients[client]} is now away')
+        self.broadcast(f'200 : {self.clients[client]} is now away')
         self.away.append(self.clients[client])
         while True:
             try:
                 next_msg = client.recv(1024).decode("ascii")
                 nxt = next_msg.split(" ",2)
                 if nxt[1] == 'BACK':
-                    self.broadcast(f'{self.clients[client]} is back')
+                    self.broadcast(f'200 : {self.clients[client]} is back')
                     break
                 elif nxt[1] == "ABS":
-                    message="You are already away"
+                    message="417 : You are already away"
                     client.send(message.encode())
                 elif nxt[1] == "CHAT":
-                    message="you can't send messages"
+                    message="403 : you can't send messages"
             except:
                 print("an error")
 
@@ -130,23 +131,54 @@ class Server:
                 message = f"{self.clients[client]} wants to have a private a chat with you.respond with ACCEPT to accept the request or REFUSE to refuse"
                 for key, valeur in self.clients.items(): 
                     if receiver == valeur: 
-                        receiver_sock = key 
-
+                        receiver_sock = key
+                while True:
+                    try:
+                        next_msg = client.recv(1024).decode("ascii")
+                        if nxt_msg == 'hm':
+                            print("hm")
+                        else:
+                            print("no hm")
+                    except:
+                        print("an error")
                 receiver_sock.send(message.encode())
 
         else:
             print(f"408 {receiver} doesn't exist")
             message = f"408 {receiver} doesn't exist"
             client.send(message.encode())
-    def refuse(self,client_to_respond,client_receiving_response):
         
+
+    '''def refuse(self,client_to_respond,client_receiving_response):
+            
+                if client_receiving_response in self.clients.values():
+                    if client_receiving_response == self.clients[client_to_respond]:
+                        message = f"{self.clients[client_to_respond]} is you, that means you can't use this command to yourself"
+                        client_to_respond.send(message.encode())
+                        
+                    else:
+                        message = f"{self.clients[client_to_respond]} refused to have a private chat with you."
+                        for key, valeur in self.clients.items(): 
+                            if client_receiving_response == valeur: 
+                                client_receiving_response_sock = key 
+
+                        client_receiving_response_sock.send(message.encode())
+                else:
+                    print(f"408 {client_receiving_response} doesn't exist")
+                    message = f"408 {client_receiving_response} doesn't exist"
+                    client_to_respond.send(message.encode())
+            
+
+        
+
+        def accept(self,client_to_respond,client_receiving_response):
             if client_receiving_response in self.clients.values():
                 if client_receiving_response == self.clients[client_to_respond]:
                     message = f"{self.clients[client_to_respond]} is you, that means you can't use this command to yourself"
                     client_to_respond.send(message.encode())
-                    
+                
                 else:
-                    message = f"{self.clients[client_to_respond]} refused to have a private chat with you."
+                    message = f"{self.clients[client_to_respond]} accepted to have a private chat with you."
                     for key, valeur in self.clients.items(): 
                         if client_receiving_response == valeur: 
                             client_receiving_response_sock = key 
@@ -155,27 +187,7 @@ class Server:
             else:
                 print(f"408 {client_receiving_response} doesn't exist")
                 message = f"408 {client_receiving_response} doesn't exist"
-                client_to_respond.send(message.encode())
-        
-
-
-    def accept(self,client_to_respond,client_receiving_response):
-        if client_receiving_response in self.clients.values():
-            if client_receiving_response == self.clients[client_to_respond]:
-                message = f"{self.clients[client_to_respond]} is you, that means you can't use this command to yourself"
-                client_to_respond.send(message.encode())
-            
-            else:
-                message = f"{self.clients[client_to_respond]} accepted to have a private chat with you."
-                for key, valeur in self.clients.items(): 
-                    if client_receiving_response == valeur: 
-                        client_receiving_response_sock = key 
-
-                client_receiving_response_sock.send(message.encode())
-        else:
-            print(f"408 {client_receiving_response} doesn't exist")
-            message = f"408 {client_receiving_response} doesn't exist"
-            client_to_respond.send(message.encode())
+                client_to_respond.send(message.encode())'''
 
     def connect(self,client):
         pass
@@ -186,10 +198,10 @@ class Server:
 
     def verify_nickname(self,newNick,client):
         if newNick in self.clients.values():
-            message=f'{newNick} is already taken'
+            message=f'409 : {newNick} is already taken'
             client.send(message.encode())
         else:       
-            self.broadcast(f'{self.clients[client]} is now {newNick}')
+            self.broadcast(f'200 : {self.clients[client]} is now {newNick}')
             for key in self.clients:
                 if key == client:
                 #if self.Connected[i]==self.clients[client]:
@@ -201,7 +213,7 @@ class Server:
         #ex=print(*self.commands,sep=", ")
         s=" ,".join(self.commands)
         print(s)
-        message=f'{s}'
+        message=f'202 : {s}'
         client.send(message.encode())
         #print(tabulate(self.com, headers=["command","definition"]))
 
@@ -211,20 +223,25 @@ class Server:
         x.sort()
         s=" ,".join(x)
         print(s)
-        message=f'{s}'
+        message=f'203 : {s}'
         client.send(message.encode())
 
 
     def quit(self,client):
-        message="you have been disconnected"
+        message="200 : you have been disconnected"
         client.send(message.encode())
         #self.Connected.remove(self.clients[client])
         name = self.clients[client]
         del self.clients[client]
-        self.broadcast(f'{name} disconnected')
+        self.broadcast(f'200 : {name} disconnected')
         self.client.close()
         
 
+'''print("server is listening ...")
+serveur = Server('127.0.0.1',9381)
+serveur.server_start()
+serveur.receive()
+'''
 port = int(input("choose a port:"))
 host = input("choose a host:")
 #host = "127.0.0.1"
